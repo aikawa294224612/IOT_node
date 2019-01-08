56b584d67136975510ba5d83e4c71d2ce83f1139
aikawaaikawa <yellowgirl3614@gmail.com>
2019-01-08 18:49:44 +0800

    20190108 ON/OFF_mqtt

---
+++ routes/mqtt_handler.js
@@ -0,0 +1,46 @@
+const mqtt = require('mqtt');
+
+class MqttHandler {
+  constructor() {
+    this.mqttClient = null;
+    this.host = 'tcp://61.219.106.176';
+    this.username = 'root'; // mqtt credentials if these are needed to connect
+    this.password = 'fanny2468';
+  }
+  
+  connect() {
+    // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
+    this.mqttClient = mqtt.connect(this.host, { username: this.username, password: this.password });
+    // this.mqttClient = mqtt.connect(this.host);
+
+    // Mqtt error calback
+    this.mqttClient.on('error', (err) => {
+      console.log(err);
+      this.mqttClient.end();
+    });
+
+    // Connection callback
+    this.mqttClient.on('connect', () => {
+      console.log('mqtt client connected');
+    });
+
+    // mqtt subscriptions
+    this.mqttClient.subscribe('mytopic', {qos: 0});
+
+    // When a message arrives, console.log it
+    this.mqttClient.on('message', function (topic, message) {
+      console.log(message.toString());
+    });
+
+    this.mqttClient.on('close', () => {
+      console.log('mqtt client disconnected');
+    });
+  }
+
+  // Sends a mqtt message to topic: mytopic
+  sendMessage(message) {
+    this.mqttClient.publish('mytopic', message);
+  }
+}
+
+module.exports = MqttHandler;
